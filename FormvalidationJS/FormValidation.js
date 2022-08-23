d9c9bf3ff910d0855226a9a70fd733d6306d9dd0
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-08-23 16:29:09 +0530

    updated

--- FormvalidationJS/FormValidation.js
+++ FormvalidationJS/FormValidation.js
@@ -1,22 +1,22 @@
 function validation1() {
     let name = document.getElementById("inputbox1").value
-    if (name == "") {
+    if (name == "" && name.length <= 4) {
         document.getElementById("span1").innerHTML = "*Please enter name"
     }
 
-}
 
-function validation1() {
-    let name = document.getElementById("inputbox2").value
-    if (name == "") {
+
+
+    let email = document.getElementById("inputbox2").value
+    if (email == "") {
         document.getElementById("span2").innerHTML = "*Please enter Email"
     }
 
-}
 
-function validation1() {
-    let name = document.getElementById("inputbox3").value
-    if (name == "") {
+
+
+    let mobile = document.getElementById("inputbox3").value
+    if (mobile == "") {
         document.getElementById("span3").innerHTML = "*Please enter mobile"
     }
 
