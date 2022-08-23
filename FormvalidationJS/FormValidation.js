6082649730fea3ef9d48c724a7e840123169b064
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-08-23 14:53:12 +0530

    Js form validation practice

---
+++ FormvalidationJS/FormValidation.js
@@ -0,0 +1,23 @@
+function validation1() {
+    let name = document.getElementById("inputbox1").value
+    if (name == "") {
+        document.getElementById("span1").innerHTML = "*Please enter name"
+    }
+
+}
+
+function validation1() {
+    let name = document.getElementById("inputbox2").value
+    if (name == "") {
+        document.getElementById("span2").innerHTML = "*Please enter Email"
+    }
+
+}
+
+function validation1() {
+    let name = document.getElementById("inputbox3").value
+    if (name == "") {
+        document.getElementById("span3").innerHTML = "*Please enter mobile"
+    }
+
+}
