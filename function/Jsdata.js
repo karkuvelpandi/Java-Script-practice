7c211475cef4d398bd197b07177d1365372cf062
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-08-23 11:43:07 +0530

    DOM callback Assignment

---
+++ function/Jsdata.js
@@ -0,0 +1,26 @@
+function operation1(){
+    document.getElementById("button1").style.backgroundColor="green"
+}
+
+function operation2(){
+    document.getElementById("button2").style.backgroundColor="yellow"
+}
+
+function operation3(){
+    document.getElementById("button3").style.backgroundColor="blue"
+}
+
+
+function operation4(){
+    document.getElementById("button4").style.backgroundColor="red"
+}
+
+function operation5() {
+    document.getElementById('input1').style.backgroundColor='aqua'
+    
+}
+
+function operation6() {
+    let name=document.getElementById('input2').value
+    console.log(name.touppercase);
+}
