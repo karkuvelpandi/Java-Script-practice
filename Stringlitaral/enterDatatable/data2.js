2388bec4729a8d1c9808e5df7d8cce12e87a8e7c
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-09-04 17:48:54 +0530

    data table

--- Stringlitaral/enterDatatable/data2.js
+++ Stringlitaral/enterDatatable/data2.js
@@ -1,1 +1,315 @@
-let data=
+let data = [
+    {
+        "id": 1,
+        "email": "khastings0@jalbum.net",
+        "Name": "Kizzee",
+        "Mobile": "190.195.190.171"
+    },
+    {
+        "id": 2,
+        "email": "lduester1@yelp.com",
+        "Name": "Lianne",
+        "Mobile": "62.87.40.155"
+    },
+    {
+        "id": 3,
+        "email": "jdommerque2@prnewswire.com",
+        "Name": "Jay",
+        "Mobile": "145.133.230.26"
+    },
+    {
+        "id": 4,
+        "email": "mfilipyev3@merriam-webster.com",
+        "Name": "Matthew",
+        "Mobile": "169.139.244.14"
+    },
+    {
+        "id": 5,
+        "email": "iboggas4@google.pl",
+        "Name": "Ibby",
+        "Mobile": "241.65.204.21"
+    },
+    {
+        "id": 6,
+        "email": "abage5@cocolog-nifty.com",
+        "Name": "Arlie",
+        "Mobile": "80.55.246.0"
+    },
+    {
+        "id": 7,
+        "email": "catterley6@businessinsider.com",
+        "Name": "Chryste",
+        "Mobile": "139.77.216.84"
+    },
+    {
+        "id": 8,
+        "email": "bfreemantle7@ibm.com",
+        "Name": "Bailey",
+        "Mobile": "115.89.16.23"
+    },
+    {
+        "id": 9,
+        "email": "amullineux8@biglobe.ne.jp",
+        "Name": "Anton",
+        "Mobile": "114.234.199.91"
+    },
+    {
+        "id": 10,
+        "email": "gbarriball9@dailymail.co.uk",
+        "Name": "Gerladina",
+        "Mobile": "70.200.224.108"
+    },
+    {
+        "id": 11,
+        "email": "rcaressa@tripadvisor.com",
+        "Name": "Rasla",
+        "Mobile": "26.131.32.86"
+    },
+    {
+        "id": 12,
+        "email": "pmullarkeyb@google.com.br",
+        "Name": "Peggie",
+        "Mobile": "12.92.26.11"
+    },
+    {
+        "id": 13,
+        "email": "hcramc@cbc.ca",
+        "Name": "Hobard",
+        "Mobile": "45.136.175.68"
+    },
+    {
+        "id": 14,
+        "email": "cellamand@mozilla.org",
+        "Name": "Cathe",
+        "Mobile": "28.254.100.20"
+    },
+    {
+        "id": 15,
+        "email": "zsetteringtone@bloomberg.com",
+        "Name": "Zulema",
+        "Mobile": "65.217.199.13"
+    },
+    {
+        "id": 16,
+        "email": "spagelsenf@linkedin.com",
+        "Name": "Shanon",
+        "Mobile": "101.164.214.150"
+    },
+    {
+        "id": 17,
+        "email": "bcrocettig@123-reg.co.uk",
+        "Name": "Brendin",
+        "Mobile": "103.152.77.83"
+    },
+    {
+        "id": 18,
+        "email": "bshallowh@myspace.com",
+        "Name": "Baxie",
+        "Mobile": "8.26.181.172"
+    },
+    {
+        "id": 19,
+        "email": "jtregeari@washington.edu",
+        "Name": "Jade",
+        "Mobile": "118.33.187.165"
+    },
+    {
+        "id": 20,
+        "email": "vannonj@livejournal.com",
+        "Name": "Vallie",
+        "Mobile": "141.14.35.28"
+    },
+    {
+        "id": 21,
+        "email": "hsherebrookek@dell.com",
+        "Name": "Hamlin",
+        "Mobile": "155.134.138.88"
+    },
+    {
+        "id": 22,
+        "email": "gwannelll@mac.com",
+        "Name": "Goddart",
+        "Mobile": "247.12.199.170"
+    },
+    {
+        "id": 23,
+        "email": "nbriskerm@answers.com",
+        "Name": "Noemi",
+        "Mobile": "183.177.63.164"
+    },
+    {
+        "id": 24,
+        "email": "adennistonn@ifeng.com",
+        "Name": "Alexandros",
+        "Mobile": "200.127.85.193"
+    },
+    {
+        "id": 25,
+        "email": "cpratleyo@free.fr",
+        "Name": "Carissa",
+        "Mobile": "96.19.228.189"
+    },
+    {
+        "id": 26,
+        "email": "lmacvicarp@chicagotribune.com",
+        "Name": "Lefty",
+        "Mobile": "134.242.226.239"
+    },
+    {
+        "id": 27,
+        "email": "vschaumannq@infoseek.co.jp",
+        "Name": "Vladamir",
+        "Mobile": "254.75.73.190"
+    },
+    {
+        "id": 28,
+        "email": "tmichalr@hatena.ne.jp",
+        "Name": "Turner",
+        "Mobile": "24.118.199.137"
+    },
+    {
+        "id": 29,
+        "email": "npirnies@ycombinator.com",
+        "Name": "Nahum",
+        "Mobile": "143.119.36.254"
+    },
+    {
+        "id": 30,
+        "email": "stacklet@zdnet.com",
+        "Name": "Stearne",
+        "Mobile": "136.66.169.43"
+    },
+    {
+        "id": 31,
+        "email": "cpudenu@twitpic.com",
+        "Name": "Colline",
+        "Mobile": "168.143.111.158"
+    },
+    {
+        "id": 32,
+        "email": "pjeckellv@ed.gov",
+        "Name": "Paule",
+        "Mobile": "236.236.56.56"
+    },
+    {
+        "id": 33,
+        "email": "hhedgerw@sogou.com",
+        "Name": "Happy",
+        "Mobile": "80.184.170.158"
+    },
+    {
+        "id": 34,
+        "email": "fmadelinx@discovery.com",
+        "Name": "Francyne",
+        "Mobile": "170.133.130.88"
+    },
+    {
+        "id": 35,
+        "email": "adennittsy@theatlantic.com",
+        "Name": "Armando",
+        "Mobile": "45.117.29.184"
+    },
+    {
+        "id": 36,
+        "email": "mjonasz@myspace.com",
+        "Name": "Montgomery",
+        "Mobile": "228.194.127.127"
+    },
+    {
+        "id": 37,
+        "email": "vmeehan10@myspace.com",
+        "Name": "Vic",
+        "Mobile": "122.219.130.125"
+    },
+    {
+        "id": 38,
+        "email": "frake11@google.co.jp",
+        "Name": "Frankie",
+        "Mobile": "242.156.126.18"
+    },
+    {
+        "id": 39,
+        "email": "rmontel12@discovery.com",
+        "Name": "Reilly",
+        "Mobile": "234.3.2.225"
+    },
+    {
+        "id": 40,
+        "email": "mbunton13@harvard.edu",
+        "Name": "Mahalia",
+        "Mobile": "227.151.239.227"
+    },
+    {
+        "id": 41,
+        "email": "ijaffa14@cafepress.com",
+        "Name": "Isa",
+        "Mobile": "105.219.112.210"
+    },
+    {
+        "id": 42,
+        "email": "pgioan15@paginegialle.it",
+        "Name": "Petronella",
+        "Mobile": "138.152.7.37"
+    },
+    {
+        "id": 43,
+        "email": "mpetrello16@independent.co.uk",
+        "Name": "Michal",
+        "Mobile": "12.66.64.159"
+    },
+    {
+        "id": 44,
+        "email": "nmowsdale17@jigsy.com",
+        "Name": "Naoma",
+        "Mobile": "178.185.63.207"
+    },
+    {
+        "id": 45,
+        "email": "wlogg18@seattletimes.com",
+        "Name": "Wittie",
+        "Mobile": "163.99.6.198"
+    },
+    {
+        "id": 46,
+        "email": "wsnelle19@ucla.edu",
+        "Name": "Wenda",
+        "Mobile": "166.213.103.70"
+    },
+    {
+        "id": 47,
+        "email": "lbacksal1a@columbia.edu",
+        "Name": "Llewellyn",
+        "Mobile": "121.113.88.91"
+    },
+    {
+        "id": 48,
+        "email": "mmyhan1b@reverbnation.com",
+        "Name": "Marcellus",
+        "Mobile": "29.219.102.110"
+    },
+    {
+        "id": 49,
+        "email": "igoligly1c@studiopress.com",
+        "Name": "Iago",
+        "Mobile": "8.99.15.158"
+    },
+    {
+        "id": 50,
+        "email": "dmcconnachie1d@cisco.com",
+        "Name": "Dru",
+        "Mobile": "20.104.103.205"
+    }
+]
+function displayData() {
+
+    let row = ""
+    for (detail of data) {
+        row = row + `<tr>
+                      <td>${detail.Name}</td>
+                     <td>${detail.email}</td>
+                     <td>${detail.id}</td>
+                     <td>${detail.Mobile.substring().trim(".")}</td>
+                     </tr>`
+    }
+    document.getElementById("id").innerHTML = row
+}
