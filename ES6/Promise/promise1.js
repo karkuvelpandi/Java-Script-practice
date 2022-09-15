62571446bbed49595748ed089239b79a5bd1b458
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-09-15 07:45:53 +0530

    callback and promise

---
+++ ES6/Promise/promise1.js
@@ -0,0 +1,22 @@
+
+let employee=[{id:101,name:"rahul",sal:12000},
+             {id:102,name:"raj",sal:13000}]
+
+
+let createEmployee=(emp)=>{
+ 
+     empployee.push(emp)
+
+
+}
+let getEmployee=()=>{
+setTimeout(()=>{
+
+    
+},3000)
+
+
+}
+
+createEmployee({id:103,name:"kamal",sal:14000})
+getEmployee()
