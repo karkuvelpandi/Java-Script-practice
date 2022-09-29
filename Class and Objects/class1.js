f538dd09816b60b70f5200a71cd4308ba9d6e2bf
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-09-29 13:20:53 +0530

    edit

--- Class and Objects/class1.js
+++ Class and Objects/class1.js
@@ -1,6 +1,37 @@
-class product{
+/* class product{
 
 }
 new product();
 
-console.log(new product);
+console.log(new product); */
+let users={
+    id:10,
+    name:4656
+}
+users.name="karkuvel"
+users.loc="bangbang"
+console.log(users);
+console.log(users.id);
+
+/* for(user in users){
+console.log(user);
+}
+
+for(use in users){
+    console.log(users[use]);
+    } */
+
+    for(use in users){
+ console.log(use);
+        console.log(users[use]);
+        }
+
+
+
+/* let arr=Object.keys(users).length
+console.log(arr);
+
+let key=Object.keys(users)
+console.log(key.length);
+
+console.log(Object.keys(users).length); */
