2060b90a474104dcdd6fd66d2ebd08fdfd37b2f1
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-11-05 22:08:57 +0530

    ok

--- ES6/ArraySupportingMethods/Map&Foreach/one.js
+++ ES6/ArraySupportingMethods/Map&Foreach/one.js
@@ -1,7 +1,18 @@
 
-let array = [10, 20, 30, 40, 50]
-                             //map iterates the array and exicute our function for array length time
-array.map((arr) => {
-    console.log(arr);    //map itrerates the array and excicute the function to give elements inside the array 
-});                       // (function can use any name to iterate)  
+// let array = [10, 20, 30, 40, 50]
+//                              //map iterates the array and exicute our function for array length time
+// array.map((arr) => {
+//     console.log(arr);    //map itrerates the array and excicute the function to give elements inside the array 
+// });                       // (function can use any name to iterate)  
 
+function main(){
+    let sum = 0;
+    let i =10;
+    while (i<1){
+        sum = sum +1
+        sum = sum *2
+        i-=1
+    }
+    console.log(sum);
+}
+main()
