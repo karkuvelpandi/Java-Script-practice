6bf17053c5ffa5ec7524fcef76fc7abac1058c68
karkuvelpandi <karkuvelpandi007@gmail.com>
2023-01-28 13:45:58 +0530

    ok

---
+++ Revision 2 @Dave/Basics/dataTypes.js
@@ -0,0 +1,8 @@
+
+console.log(typeof"dave");
+//************************** */
+
+//Boolean Data type
+let myNumber =88
+let myBool=myNumber===88
+console.log(typeof myBool); 
---
+++ Revision 2 @Dave/Chapter-03-String/one.js
@@ -0,0 +1,6 @@
+//string
+let myVariable="karkuvel pandi"
+
+//length property
+console.log(myVariable.length); 
+console.log("myVariable".length); 
