a1825679898e4b77ed2e5aa48188400928a3949a
karkuvelpandi <karkuvelpandi007@gmail.com>
2023-01-29 13:36:20 +0530

    ok

---
+++ Revision 2 @Dave/c-5-math-mehods.js
@@ -0,0 +1,24 @@
+// Inbuilt Math method
+console.log(Math.PI);  //3.141592653589793
+
+// Math.trunc()-method it return Integer portion of a number
+
+console.log(Math.trunc(Math.PI)); //3
+
+//Math.round() will round the value and give as a number datatype only
+console.log( Math.round(3.14)); // number 3
+console.log( Math.round("3.14")); // number 3
+console.log( Math.round("3.54")); // number 4
+console.log( Math.round("pk")); // NaN
+console.log( Math.round(undefined)); // NaN
+console.log( Math.round(null)); // 0
+console.log( Math.round(true)); // 1
+console.log( Math.round(false)); // 0
+
+// Math.ceil()-It will give us incremental side round value
+console.log( Math.ceil("3.14")); // number 4
+
+// Math.floor()-It will give us decremental side round value
+console.log( Math.floor("3.8")); // number 3
+
+
